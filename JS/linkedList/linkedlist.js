aab6807ea38fd9ea74d33961ae370bd8bf55b2dc
Md Rizwan Akram <akramrizwan8@gmail.com>
2021-01-16 17:44:16 +0530

    Adds nth position insertion in linkedlist JS

--- JS/linkedList/linkedlist.js
+++ JS/linkedList/linkedlist.js
@@ -21,11 +21,33 @@ class LinkedList {
     newNode.next = this.head;
     this.head = newNode;
   }
+  insertAtNthPositon(data, n) {
+    let newNode = new Node(data);
+    if (n == 1) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+    let temp = this.head;
+    for (let i = 1; i < n - 1; i++) {
+      //   console.log(temp.data);
+      temp = temp.next;
+    }
+    newNode.next = temp.next;
+    temp.next = newNode;
+  }
 }
 
 const llist = new LinkedList();
-
+// prepend
 llist.prepend(10);
 llist.prepend(20);
 llist.prepend(14);
+
+// insert at nth positions
+llist.insertAtNthPositon(3, 1);
+llist.insertAtNthPositon(2, 2);
+llist.insertAtNthPositon(1, 3);
+llist.insertAtNthPositon(11, 3);
+
 llist.printList();
