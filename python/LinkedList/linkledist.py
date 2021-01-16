4358146c48f5ace4524fc101a664e9b05e6dfcd7
Md Rizwan Akram <akramrizwan8@gmail.com>
2021-01-16 18:04:44 +0530

    Add insert_in_nth_position linkedlist python

--- python/LinkedList/linkledist.py
+++ python/LinkedList/linkledist.py
@@ -8,17 +8,31 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def printList(self):
+    def print_list(self):
         temp = self.head
         while temp:
             print(temp.data)
             temp = temp.next
 
     # time complexity O(1)
-    def addBeginning(self, data):
+    def prepend(self, data):
         newNode = Node(data)
         newNode.next = self.head
         self.head = newNode
+
+    def insert_at_nth_pos(self, data, n):
+        newNode = Node(data)
+        if n == 1:
+            newNode.next = self.head
+            self.head = newNode
+            return
+
+        temp = self.head
+        for i in range(1, n - 1):
+            temp = temp.next
+
+        newNode.next = temp.next
+        temp.next = newNode
 
 
 if __name__ == "__main__":
@@ -30,7 +44,16 @@ if __name__ == "__main__":
 
     # llist.head.next = second
     # second.next = third
-    llist.addBeginning(20)
-    llist.addBeginning(31)
-    llist.addBeginning(45)
-    llist.printList()
+
+    # prepend
+    # llist.prepend(20)
+    # llist.prepend(31)
+    # llist.prepend(45)
+
+    # insert at nth position
+    llist.insert_at_nth_pos(4, 1)  # List: 4
+    llist.insert_at_nth_pos(3, 1)  # List: 3 4
+    llist.insert_at_nth_pos(5, 3)  # List  3, 4, 5
+    llist.insert_at_nth_pos(6, 2)  # List  3, 6, 4, 5
+
+    llist.print_list()
